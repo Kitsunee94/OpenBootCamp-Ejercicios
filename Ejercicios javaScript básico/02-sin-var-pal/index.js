8e19307fc65bec5438495f4791d714d016010d7c
Kitsunee94 <117470142+Kitsunee94@users.noreply.github.com>
2022-12-23 18:10:39 +0100

    Add files via upload

---
+++ Ejercicios javaScript básico/02-sin-var-pal/index.js
@@ -0,0 +1,12 @@
+let Nombre =["Carles"]
+let Edad =[28]
+let Desarollador =[false]
+const Nacimiento = new Date("July 05 1994")
+const Libro ={
+    title:"El món groc",
+    autor:"Albert Espinosa",
+    fecha:2008,
+    url:"https://acortar.link/EG3XmB",
+}
+const lista =[Nombre,Edad,Desarollador,Nacimiento,Libro]
+console.log(lista)
