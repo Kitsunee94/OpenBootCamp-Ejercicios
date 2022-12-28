3302312ec01f5d68ceaa6de4b519a804532e19ce
Kitsunee94 <117470142+Kitsunee94@users.noreply.github.com>
2022-12-28 13:21:16 +0100

    Add files via upload

---
+++ Ejercicios javaScript básico/02-sin-var-pal/index.js
@@ -0,0 +1,14 @@
+const lista = [
+    "Carles",
+    28,
+    false,
+    fecha_valores = new Date(1994, 6,5),
+    {
+        titulo: "Harry potter y la piedra filosofal",
+        autor: "J.K.Rowling",
+        fecha: fecha_valores = new Date(1997,5,26),
+        url: "amzn.eu/d/4TMzifY"
+    },
+];
+
+console.log(lista)
