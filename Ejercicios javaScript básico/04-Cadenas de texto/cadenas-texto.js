3ff415f27635cabc2645270ac5b5a8d8147da8e0
Kitsunee94 <117470142+Kitsunee94@users.noreply.github.com>
2022-12-30 13:08:12 +0100

    Add files via upload

---
+++ Ejercicios javaScript básico/04-Cadenas de texto/cadenas-texto.js
@@ -0,0 +1,16 @@
+let nombre = "Carles";
+let apellido = "Brugada";
+let estudiante =`${nombre} ${apellido}`;
+let estudianteMayus = "estudianteMayus";
+let estudianteMinus = "estudianteMinus";
+
+console.log(nombre)
+console.log(apellido)
+console.log(estudiante.concat())
+console.log(estudianteMayus.toUpperCase())
+console.log(estudianteMinus.toLowerCase())
+console.log(estudiante.length)
+console.log(nombre[0])
+console.log(apellido[0])
+console.log(estudiante.trim().length)
+console.log(estudiante.includes("Carles"))
