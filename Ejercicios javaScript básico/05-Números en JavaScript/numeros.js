73583b7f2b350a4cd568facfa0b6eb5908b5c37f
Kitsunee94 <117470142+Kitsunee94@users.noreply.github.com>
2022-12-30 16:46:24 +0100

    Add files via upload

---
+++ Ejercicios javaScript básico/05-Números en JavaScript/numeros.js
@@ -0,0 +1,11 @@
+
+let alturacm = 170
+let alturam = 1.70
+let peso = 100.1
+
+console.log(Math.ceil(alturam))
+
+console.log (Math.floor(peso))
+
+let igualdad = Number.MAX_VALUE +1 === Number.MAX_VALUE
+console.log(igualdad)
