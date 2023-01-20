035b9d1fc75d155f82adeaeff22cd5e23c5293f8
Kitsunee94 <117470142+Kitsunee94@users.noreply.github.com>
2023-01-20 15:03:08 +0100

    Add files via upload

---
+++ Ejercicios curso Java Básico/Ejercicio1/src/com/ejercicio/Ejercicio.java
@@ -0,0 +1,24 @@
+package com.ejercicio;
+
+public class Ejercicio {
+    public static void main(String[] args) {
+        char letra = 'a';
+        System.out.println(letra);
+        boolean cierto = true;
+        System.out.println(cierto);
+        byte numero1 = 1;
+        System.out.println(numero1);
+        int numero2 = 1;
+        System.out.println(numero2);
+        long numero3 = 2;
+        System.out.println(numero3);
+        short numero4 = 3;
+        System.out.println(numero4);
+        float numero5 = 4.4f;
+        System.out.println(numero5);
+        double numero6 = 2.2d;
+        System.out.println(numero6);
+        String frase ="Hola mundo";
+        System.out.println(frase);
+    }
+}
