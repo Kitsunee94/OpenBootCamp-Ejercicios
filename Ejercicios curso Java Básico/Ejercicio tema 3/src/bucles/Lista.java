a252b1ab94d6271f93031d07d4a731003762166b
Kitsunee94 <117470142+Kitsunee94@users.noreply.github.com>
2023-01-23 13:30:52 +0100

    Add files via upload

---
+++ Ejercicios curso Java Básico/Ejercicio tema 3/src/bucles/Lista.java
@@ -0,0 +1,10 @@
+package bucles;
+
+public class Lista {
+    public static void main(String[] args) {
+        String[] nombres = {"Carles", "Bea", "Joan", "Maria", "Pau"};
+        for (String nombre : nombres) {
+            System.out.println(nombre);
+        }
+    }
+}
